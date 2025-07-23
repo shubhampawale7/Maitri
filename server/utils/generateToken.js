e6146ebff06aef8c50c95f4ed38129cb3fdc6f6f
shubhampawale7 <shubhampawale7@gmail.com>
2025-07-23 11:47:32 +0530

    updated the jwt fixation

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -7,8 +7,8 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-    sameSite: "strict", // Prevent CSRF attacks
+    secure: process.env.NODE_ENV !== "development", // true on Vercel/Render
+    sameSite: "None", // ✅ Required for cross-origin cookies
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
