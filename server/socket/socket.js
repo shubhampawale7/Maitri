79dbc10a51a060cd127c7813e721da6cde552c2f
shubhampawale7 <shubhampawale7@gmail.com>
2025-07-23 09:57:49 +0530

    Correct production URLs and CORS policies

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -7,7 +7,7 @@ const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     methods: ["GET", "POST"],
     credentials: true,
   },
