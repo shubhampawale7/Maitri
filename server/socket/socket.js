f25d85448b670e9b6d914e9f1c75888dabced7f8
shubhampawale7 <shubhampawale7@gmail.com>
2025-07-23 10:11:11 +0530

    refactored socket.js and server.js

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import initializeSocket from "./socketHandler.js";
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -13,7 +14,9 @@ const io = new Server(httpServer, {
   },
 });
 
-export const userSocketMap = new Map(); // {userId: socketId}
+initializeSocket(io);
+
+export const userSocketMap = new Map();
 
 export const getReceiverSocketId = (receiverId) => {
   return userSocketMap.get(receiverId);
